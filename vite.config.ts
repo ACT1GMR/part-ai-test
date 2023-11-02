3f32dede039eb2c2abc023546bb905aaf68116cf
ACT1GMR <act1gmr@googlemail.com>
2023-11-02 19:35:03 +0330

    - init

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: 'http://localhost:63342/foo/dist/',
-  plugins: [react()],
-})
